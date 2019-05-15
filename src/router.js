dc94d76f2c1c23baefd02b9447c489dadc24c71e
Phil Albu <phil.albu@gmail.com>
2019-05-14 22:50:34 -0500

    my first commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ThreadShow from '@/components/ThreadShow'
+import HelloWorld from './components/HelloWorld'
 
 Vue.use(Router)
 
@@ -11,7 +12,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: HelloWorld
     },
     {
       path: '/about',
@@ -20,6 +21,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
     }
   ]
 })
